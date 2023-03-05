a328ef7156653e1a04e213e7d6d986dbe4fbe2cc
Sean Gombart <skgombart@gmail.com>
2023-03-04 18:38:01 -0800

    Added motivational quote cycler

--- src/DOMchanges.js
+++ src/DOMchanges.js
@@ -1,18 +1,35 @@
-import {minutes, seconds} from './timer.js'
+import { minutes, seconds } from "./timer.js";
+
+// Global Variables
+const quotes = document.getElementById("quotes");
+let quoteList = [
+  "There are no secrets to success. It is the result of preparation, hard work, and learning from failure.",
+  "I am who I am today because of the choices I made yesterday.",
+  "The only thing standing between you and outrageous success is continuous progress.",
+  "I'm a great believer in luck, and I find the harder I work the more I have of it.",
+  "If you really look closely, most overnight successes took a long time.",
+  "Setting goals is the first step in turning the invisible into the visible.",
+  "Impossible is just an opinion.",
+  "The greater the difficulty, the more the glory in surmounting it.",
+  "You are your greatest asset. Put your time, effort and money into training.",
+];
 
 function setStudyTime() {
-    minutes.textContent = '25';
-    seconds.textContent = '00';
+  minutes.textContent = "25";
+  seconds.textContent = "00";
 }
 
 function setShortBreak() {
-    minutes.textContent = '05';
-    seconds.textContent = '00';
-};
+  minutes.textContent = "05";
+  seconds.textContent = "00";
+}
 
 function setLongBreak() {
-    minutes.textContent = '20';
-    seconds.textContent = '00';
-};
+  minutes.textContent = "20";
+  seconds.textContent = "00";
+}
 
-export {setStudyTime, setShortBreak, setLongBreak};
+function changeQuote() {
+  quotes.textContent = quoteList[Math.floor(Math.random() * quoteList.length)];
+}
+export { setStudyTime, setShortBreak, setLongBreak, changeQuote, quotes };
--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-import {startFunction, start} from './timer.js'
-import {setStudyTime, setShortBreak, setLongBreak} from './DOMchanges.js'
+import { startFunction, start } from "./timer.js";
+import {
+  setStudyTime,
+  setShortBreak,
+  setLongBreak,
+  changeQuote,
+} from "./DOMchanges.js";
 
 //Global Variable
-const startBtn = document.getElementById('start');
-const stopBtn = document.getElementById('stop');
-const study = document.querySelector('.study');
-const shortBreak = document.querySelector('.shortBreak');
-const longBreak = document.querySelector('.longBreak');
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const study = document.querySelector(".study");
+const shortBreak = document.querySelector(".shortBreak");
+const longBreak = document.querySelector(".longBreak");
 
 // Event Listener
-startBtn.addEventListener('click', () => {
-    console.log('clicked');
-    startBtn.disabled = true;
-    startFunction();
+startBtn.addEventListener("click", () => {
+  console.log("clicked");
+  startBtn.disabled = true;
+  startFunction();
 });
 
-stopBtn.addEventListener('click', () => {
-    console.log('stopClick');
-    startBtn.disabled = false;
-    clearInterval(start);
+stopBtn.addEventListener("click", () => {
+  console.log("stopClick");
+  startBtn.disabled = false;
+  clearInterval(start);
 });
 
-study.addEventListener('click', () => setStudyTime());
+study.addEventListener("click", () => setStudyTime());
 
-shortBreak.addEventListener('click', () => setShortBreak());
+shortBreak.addEventListener("click", () => setShortBreak());
 
-longBreak.addEventListener('click', () => setLongBreak());
+longBreak.addEventListener("click", () => setLongBreak());
 
-
+//Functions
+setInterval(changeQuote, 5000);
