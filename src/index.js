584a4b74d90791cf5ed1f5a50352209029b426a2
Sean Gombart <skgombart@gmail.com>
2023-03-04 16:25:44 -0800

    Added event listeners to start and stop timer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import {test} from './timer.js'
+import {startFunction, start} from './timer.js'
 
+//Global Variable
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
 
-console.log("Hello from index");
+// Event Listener
+startBtn.addEventListener('click', () => {
+    console.log('clicked');
+    startBtn.disabled = true;
+    startFunction();
+});
 
-test();
+stopBtn.addEventListener('click', () => {
+    console.log('stopClick');
+    startBtn.disabled = false;
+    clearInterval(start);
+});
