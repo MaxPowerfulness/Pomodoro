c80b1974a58287af3740a798fd436de91083a3bc
Sean Gombart <skgombart@gmail.com>
2023-03-07 22:00:19 -0800

    Added alarm when time runs out

--- src/timer.js
+++ src/timer.js
@@ -1,30 +1,43 @@
 // Global variables
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
-let start = null; 
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+let start = null;
+const alarm = new Audio("../Ping1.mp3");
+
 //Functions
 function countdown() {
-    console.log('minutes.textContent', minutes.textContent);
-    console.log('seconds.textContent', seconds.textContent);
-    if ((minutes.textContent == '00') & (seconds.textContent == '00')) {
-        console.log('clearRuns');
-        clearInterval(start);
-    } else if (seconds.textContent == '00') {
-        seconds.textContent = '59';
-        if (minutes.textContent != '00') {
-            minutes.textContent = (parseInt(minutes.textContent) - 1).toString()
-        };
+  console.log("minutes.textContent", minutes.textContent);
+  console.log("seconds.textContent", seconds.textContent);
+  if ((minutes.textContent == "00") & (seconds.textContent == "00")) {
+    clearInterval(start);
+    playSound();
+  } else if (seconds.textContent == "00") {
+    seconds.textContent = "59";
+    if (minutes.textContent != "00") {
+      minutes.textContent = (parseInt(minutes.textContent) - 1).toString();
+    }
+  } else {
+    if (parseInt(seconds.textContent) <= 10) {
+      seconds.textContent = (parseInt(seconds.textContent) - 1)
+        .toString()
+        .padStart(2, "0");
     } else {
-        if (parseInt(seconds.textContent) <= 10) {
-            seconds.textContent = (parseInt(seconds.textContent) - 1).toString().padStart(2, '0');
-        } else {
-            seconds.textContent = (parseInt(seconds.textContent) - 1).toString();
-        };
-    };
-};
+      seconds.textContent = (parseInt(seconds.textContent) - 1).toString();
+    }
+  }
+}
 
 function startFunction() {
-    start = setInterval(countdown, 1000);
-};
+  start = setInterval(countdown, 1000);
+}
 
-export {startFunction, start};
+function playSound() {
+  alarm.play();
+}
+
+function stopSound() {
+  alarm.pause();
+  alarm.currentTime = 0;
+}
+
+export { startFunction, stopSound, start, minutes, seconds };
